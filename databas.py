0e9a93ab85894784af97d35d21bbbab362473523
ShivaMoradi <shiva.moradi1@gmail.com>
2024-01-26 11:47:26 +0100

    added some modification on main and also identified id field in database

--- databas.py
+++ databas.py
@@ -1,14 +1,13 @@
 import mysql.connector
-
 db = mysql.connector.connect(
 
     host="localhost", user="root",
-    password="mypassword", database="E-handelSystem"
+    password="mypassword", database="Yatzy"
 )
 cursor = db.cursor(dictionary=True)
 
 
 def save_player_info(player, player_score):
-    cursor.execute(f"INSERT INTO players (player, player_score)"
-                   f"VALUES ('{player}', '{player_score}')")
+    cursor.execute(f"INSERT INTO Scores (id, player, player_score)"
+                   f"VALUES (NULL, '{player}', {player_score})")
     db.commit()
--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import numpy as np
 import databas
 import random
 
-Player_scores = {'player': '<player>', 'player_score': '<player_score>'}
-
 
 class Yatzy:
     def __init__(self, player, player_score):
@@ -12,23 +10,50 @@ class Yatzy:
 
     def start(self):
         game_running = True
+        rounds_played = 0
+        players = []
+
         while game_running:
-            self.player = input("Enter player name: ")
-            self.player_score = 0
-            print("current score for the player is: ", self.player_score)
+            rounds_played += 1
+            print(f"you are in round: {rounds_played}")
             dic_rolls = np.array([])
             for i in range(5):
                 cast_dice = random.randint(1, 6)
-                dic_rolls.append(cast_dice)
+                dic_rolls = np.append(dic_rolls, cast_dice)
                 answer = input("Do you want to save the score? y/n")
                 if answer == "y":
                     self.player_score = np.sum(dic_rolls)
                     databas.save_player_info(player=self.player, player_score=self.player_score)
                     break
                 elif answer.lower() == "n":
-                    print("Roll again:", dic_rolls)
+                    print(f"Roll again: {dic_rolls}")
+        self.check_currecnt_status(players)
+        play_again = input("Do you want to play again? y/n")
+        if play_again.lower() != "y":
             game_running = False
 
+    def check_currecnt_status(self, players):
+        print("\nCurrent Standings:")
+        for player_info in players:
+            player, player_score = player_info
+            print(f"Player: {player}, Total Score: {player_score}")
 
-yatzy_instance = Yatzy(player=Player_scores['player'], player_score=Player_scores['player_score'])
+        # Determine last and first place
+        sorted_players = sorted(players, key=lambda x: x[1])
+        last_place, first_place = sorted_players[0], sorted_players[-1]
+
+        print(f"\nLast Place: Player {last_place[0]} with score {last_place[1]}")
+        print(f"First Place: Player {first_place[0]} with score {first_place[1]}")
+
+    if __name__ == "__main__":
+        players = []
+
+        # Add players dynamically
+        num_players = int(input("Enter the number of players: "))
+        for _ in range(num_players):
+            player_name = input("Enter player name: ")
+            players.append((player_name, 0))
+
+
+yatzy_instance = Yatzy(player="", player_score=0)
 yatzy_instance.start()
